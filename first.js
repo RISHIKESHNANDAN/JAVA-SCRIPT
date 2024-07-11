3e91517fe86bf7608cf4d426a8b8926443b88b6c
RISHIKESH NANDAN <rishikesh082005@gmail.com>
2024-07-11 22:10:08 +0530

    unary operater

--- first.js
+++ first.js
@@ -1,4 +1,4 @@
-//Artimetic Operators
+//Arithmetic Operators
 let a = 46;
 let b = 23;
 
@@ -8,3 +8,10 @@ console.log(" a + b = " , a + b);
 console.log(" a - b = " , a - b);
 console.log(" a * b = " , a * b);
 console.log(" a / b = " , a / b);
+console.log(" a % b = " , a % b);
+console.log(" a ** b = " , a ** b);
+//Unary operater
+console.log(" a++ ", a++);
+console.log( a );
+console.log(" a-- " , a--);
+console.log( a-- )
